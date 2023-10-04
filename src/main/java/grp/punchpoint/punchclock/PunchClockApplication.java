f0e1e24d61f5c7a22b693861ce38eb2b6c8ca31a
LMTech <biaoche1hao@126.com>
2023-10-04 12:56:00 -0400

    config server & docker

--- src/main/java/grp/punchpoint/punchclock/PunchClockApplication.java
+++ src/main/java/grp/punchpoint/punchclock/PunchClockApplication.java
@@ -2,6 +2,7 @@ package grp.punchpoint.punchclock;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 
 @SpringBootApplication
 public class PunchClockApplication {
@@ -9,5 +10,4 @@ public class PunchClockApplication {
     public static void main(String[] args) {
         SpringApplication.run(PunchClockApplication.class, args);
     }
-
 }
